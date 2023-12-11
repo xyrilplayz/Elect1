bccc5243a729d1df28582cc06fe8d5ebb59d4219
xyrilplayz <xyrilplayz@gmail.com>
2023-12-11 15:55:17 +0800

    getter and setter

---
+++ first getter and setter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AseanPhonebook
+{
+    class Student
+    {
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+        public char Gender { get; set; }
+        public string CountryCode { get; set; }
+        public string AreaCode { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CountryCode}-{AreaCode} {LastName}, {FirstName}, {PhoneNumber}, {StudentID}";
+        }
+    }
+}
